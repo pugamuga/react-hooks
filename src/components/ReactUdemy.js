3ba6615fcad1a786b8bfe78d6ff63abba984458e
Evgeny Pugachev <rabota.pugamuga@gmail.com>
2022-09-09 09:54:19 +0300

    continue

--- src/components/ReactUdemy.js
+++ src/components/ReactUdemy.js
@@ -2,10 +2,15 @@ import React from "react";
 import { useState } from "react";
 
 const UserMade = ({ text }) => <div className="mt-2 ">{text}</div>;
+const headerText = "Here ReactUdemy div";
+
+const ChildrenText = ({ children }) => {
+  return <div className="p-1">{children}</div>;
+};
 
 const ReactUdemy = () => {
   const [margin, setMargin] = useState(false);
-  console.log(margin)
+  console.log(margin);
   const arEmpty = [];
   for (let i = 0; i < 10; i++) {
     arEmpty.push(i);
@@ -13,15 +18,20 @@ const ReactUdemy = () => {
   return (
     <>
       <div
-        className=" cursor-pointer border-2 px-2 rounded-full w-[200px] active:opacity-50"
+        className=" cursor-pointer border-2 px-2 rounded-md w-1/4 active:opacity-50"
         onClick={() => setMargin((prev) => !prev)}
       >
-        Here ReactUdemy div
+        {" "}
+        <div className="flex flex-col">
+          <h1>{headerText.toLowerCase()}</h1>
+          <h1>{headerText.toUpperCase()}</h1>
+          <h1>{headerText.toString()}</h1>
+        </div>
       </div>
       {arEmpty.map((item, i) => {
         return (
           <div
-          className=" transition-all duration-500 "
+            className=" transition-all duration-500 "
             key={i}
             style={{
               backgroundColor: `rgb(255,${10 + 20 * i},${10 + 20 * i})`,
@@ -30,7 +40,12 @@ const ReactUdemy = () => {
               marginLeft: `${margin ? 20 * i : 0}px`,
             }}
           >
-            <UserMade text={`It's ${i + 1} component`} />
+            <UserMade text={`It's ${i + 1} component, index is ${i}`} />
+            <div>
+              <ChildrenText>
+                {i === 9 || i == 4 || i == 0 ? <input type="text" /> : null}
+              </ChildrenText>
+            </div>
           </div>
         );
       })}
