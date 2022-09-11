647a7260b2796e82ecd710e248043e9adbaa5d24
Evgeny Pugachev <rabota.pugamuga@gmail.com>
2022-09-11 23:50:21 +0300

    useContext done

--- src/components/useContext/Book.js
+++ src/components/useContext/Book.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import Context from './Context'
 
 const Book = ({num}) => {
+
+const value = useContext(Context)
+
   return (
     <div className='h-[10vw] w-[10vw] flex flex-col items-center p-4 justify-between bg-emerald-400 rounded-lg'>
         <h1>Buy {num} {num==1?"book":"books"}</h1>
         <h1 className='text-4xl drop-shadow-lg'>📚</h1>
-        <button className=' bg-sky-800 active:opacity-50 shadow-md py-1 w-full rounded-full'>Buy</button>
+        <button
+        onClick={() => {
+            value.buyBook(num)
+        }}
+        className=' bg-sky-800 active:opacity-50 shadow-md py-1 w-full rounded-full'>Buy</button>
     </div>
   )
 }
--- src/components/useContext/BookStore.js
+++ src/components/useContext/BookStore.js
@@ -1,17 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 import AllBooks from "./AllBooks";
+import Context from "./Context";
 
 const BookStore = () => {
+  const [amountBooks, setAmountBooks] = useState(0);
+
+  const buyBook = (n) => {
+    return setAmountBooks((prev) => {
+      return prev + n;
+    });
+  };
+
+  const valueBook = {
+    amountBooks,
+    buyBook,
+  };
+
   return (
-    <div className="flex justify-center items-center h-[30vw]">
-      <div className=" bg-white rounded-md w-2/3 p-4 gap-y-12 flex flex-col items-center justify-between ">
-        <h1 className="text-slate-600 text-4xl">Book Store</h1>
-        <h1 className="text-slate-400 text-2xl">
-          You already buy {NaN} books📖
-        </h1>
-        <AllBooks />
+    <Context.Provider value={valueBook}>
+      <div className="flex justify-center items-center h-[30vw]">
+        <div className=" bg-white rounded-md w-2/3 p-4 gap-y-12 flex flex-col items-center justify-between ">
+          <h1 className=" text-4xl bg-emerald-400 w-full text-center py-2 rounded-md">
+            Book Store
+          </h1>
+          <div className="flex flex-row ">
+            <h1 className="text-slate-400 text-2xl">
+              You already buy {amountBooks}{" "}
+              {amountBooks == 1 ? "book" : "books"}
+              📖
+            </h1>
+            {amountBooks > 0 && <button
+            onClick={() => {
+                setAmountBooks(() => {
+                    return 0
+                })
+            }}
+            className="btn bg-rose-500 ml-4">Clear</button>}
+          </div>
+          <AllBooks />
+        </div>
       </div>
-    </div>
+    </Context.Provider>
   );
 };
 
